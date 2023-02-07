dbcb6948624bf698806b708eb3558335b90a78b0
James Albright <G00379271@atu.ie>
2023-02-07 16:48:18 +0000

    Created v5 of Product package Added all previous files Added new Music class to handle music products

---
+++ src/main/java/ie/atu/Productv5/Music.java
@@ -0,0 +1,32 @@
+package ie.atu.Productv5;
+
+public class Music extends Product {
+
+    private String artist, label;
+
+    public Music() {
+        super();
+        artist = "";
+        label = "";
+        count++;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    @Override
+    public String toString() {return super.toString() + " by " + artist + "\nPart of " + label;}
+}
---
+++ src/main/java/ie/atu/Productv5/ProductDB.java
@@ -0,0 +1,90 @@
+package ie.atu.Productv5;
+
+public class ProductDB {
+
+    public static Product getProduct(String productCode) {
+        // In a more realistic application, this code would
+        // get the data for the product from a file or database
+        // For now, this code just uses if/else statements
+        // to return the correct product data
+
+        Product p = null;
+
+        if (productCode.equalsIgnoreCase("java"))
+        {
+           Book myBook = new Book();
+            myBook.setCode(productCode);
+            myBook.setDescription("ATU Java Programming");
+            myBook.setPrice(57.50);
+            myBook.setAuthor("Joe Brown");
+            p = myBook;
+        } else if (productCode.equalsIgnoreCase("jsp")) {
+            Book myBook = new Book();
+            myBook.setCode(productCode);
+            myBook.setDescription("Java Servlets and JSP");
+            myBook.setPrice(57.50);
+            myBook.setAuthor("Mike White");
+            p = myBook;
+        } else if (productCode.equalsIgnoreCase("mysql")) {
+            Book myBook = new Book();
+            myBook.setCode(productCode);
+            myBook.setDescription("Lennon's MySQL");
+            myBook.setPrice(54.50);
+            myBook.setAuthor("Jim Lennon");
+            p = myBook;
+        }
+
+
+        if (productCode.equalsIgnoreCase("studios")) {
+            Software mySoftware = new Software();
+            mySoftware.setCode(productCode);
+            mySoftware.setDescription("Visual Studios");
+            mySoftware.setPrice(57.50);
+            mySoftware.setVersion("Microsoft 1.1");
+            p = mySoftware;
+        } else if (productCode.equalsIgnoreCase("eclipse")) {
+            Software mySoftware = new Software();
+            mySoftware.setCode(productCode);
+            mySoftware.setDescription("Build Java apps");
+            mySoftware.setPrice(57.50);
+            mySoftware.setVersion("Eclipse Neon");
+            p = mySoftware;
+        } else if (productCode.equalsIgnoreCase("oracle")) {
+            Software mySoftware = new Software();
+            mySoftware.setCode(productCode);
+            mySoftware.setDescription("Latest MySQL");
+            mySoftware.setPrice(54.50);
+            mySoftware.setVersion("Oracle 3.0");
+            p = mySoftware;
+        }
+
+        if (productCode.equalsIgnoreCase("pink")) {
+            Music myMusic = new Music();
+            myMusic.setCode(productCode);
+            myMusic.setDescription("Wish you were here");
+            myMusic.setPrice(8.00);
+            myMusic.setLabel("the Columbia group");
+            myMusic.setArtist("Pink Floyd");
+            p = myMusic;
+        } else if (productCode.equalsIgnoreCase("nirvana")) {
+            Music myMusic = new Music();
+            myMusic.setCode(productCode);
+            myMusic.setDescription("Come as you are");
+            myMusic.setPrice(7.00);
+            myMusic.setLabel("Geffen Records");
+            myMusic.setArtist("Nirvana");
+            p = myMusic;
+        } else if (productCode.equalsIgnoreCase("one")) {
+            Music myMusic = new Music();
+            myMusic.setCode(productCode);
+            myMusic.setDescription("One");
+            myMusic.setPrice(10.00);
+            myMusic.setLabel("Blackened Recordings");
+            myMusic.setArtist("Metallica");
+            p = myMusic;
+        }
+
+        return p;
+    }
+}
+
